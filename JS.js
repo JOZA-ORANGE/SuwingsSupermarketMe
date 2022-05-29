1021ad154b610450fe2504f4d9de24cf7614b137
JOZA ORANGE <973413409@qq.com>
2022-05-29 22:30:20 +0800

    新东西

---
+++ JS.js
@@ -0,0 +1,7 @@
+console.log(`
+ %c SupermarketMe %c Version:0.0.2 %c BY JOZA 
+
+`,"color: #000; background: rgb(255,239,213); padding:5px 0px;","background:rgba(200, 200, 200, 0.75); padding:5px 0px;","color: #ffffff; background: rgba(114, 51, 4, 0.85); padding:5px 0px;");
+console.log(`
+ %c Github %c https://github.com/JOZA-ORANGE/SuwingsSupermarketMe %c
+ `,"color: #fff; background: #000; padding:5px 0;border:1px solid black","color: #000;background:#fff ; padding:5px 0; border:1px solid black;");
